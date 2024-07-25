9f48eb120ea7b35572692601b6b804d56a00ee1e
Rahul Tak <rahultak955@gmail.com>
2024-07-25 23:59:19 +0530

    api upload in images

--- api-calling/src/Components/UserCard.jsx
+++ api-calling/src/Components/UserCard.jsx
@@ -4,7 +4,7 @@ const UserCard = (props) => {
     console.log(props.data)
   return (
     <div className='user-card'>
-      <img className='user-img' src={props.data.picture} />
+      <img className='user-img' src={props.data.picture.thumbnail} />
       <h3>name{props.data.name.first}</h3>
       <h4>+91 {props.data.phone}</h4>
       <p>City {props.data.location.city}</p>
