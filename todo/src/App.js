54ed7f8c904176d922cc2b26542ec5efd4b3ae3a
Rahul Tak <rahultak955@gmail.com>
2024-08-02 11:33:04 +0530

    todo list create

---
+++ todo/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header'
+import TodoItem from './components/TodoItem';
+import Button from './components/Button';
+
+import './style.css'
+
+function App() {
+  return (
+    <div className="todo-container">
+     <Header/>
+     <TodoItem/>
+     <Button/>
+    </div>
+  );
+}
+
+export default App;
