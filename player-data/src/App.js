dd15db7c8ce1d11bf58ac307baec18a581141a68
Rahul Tak <rahultak955@gmail.com>
2024-11-10 20:26:53 +0530

    update

--- player-data/src/App.js
+++ player-data/src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '3e5dda6434mshb5669669f357ca0p177947jsn56f158e5ab6a',
+        'x-rapidapi-host': 'cricbuzz-cricket.p.rapidapi.com',
+      }
+    };
+
+    fetch('https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/search?plrN=Tucker', options)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
